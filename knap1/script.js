a33aa54e45479da7ef1dbccf1df529868f03c47e
Mads R. A <madsmeda@gmail.com>
2021-03-03 14:51:39 +0100

    øvelse til knap 1 + 2

---
+++ knap1/script.js
@@ -0,0 +1,24 @@
+console.log("'mainscript.js is running...'");
+
+document.getElementById("knap").addEventListener("click", hello);
+document.getElementById("knap2").addEventListener("click", clear);
+
+function hello () {
+    console.log("hej");
+}
+
+function clear () {
+    console.clear();
+}
+
+/*
+Function expression
+let hello = function () {
+    console.log("hello");
+}
+
+Arrow function
+let hello = () => {
+    console.log("hello");
+}
+*/
---
+++ knap2/script.js
@@ -0,0 +1,27 @@
+console.log("'mainscript.js is running...'");
+
+function hello () {
+    for (i = 0; i < 3; i++) {
+        console.log("hello");
+    }
+}
+
+function clearConsole () {
+    console.clear();
+}
+
+/*
+Function expression
+let hello = function () {
+    console.log("hello");
+}
+
+Arrow function
+let hello = () => {
+    console.log("hello");
+}
+*/
+
+/*
+https://www.youtube.com/watch?v=4NfFFsQC77M
+*/
